e79e8f6d00fe4e086af20d2a741d2359ccf4f9bb
Juan P <pront23@gmail.com>
2019-06-06 13:32:11 +0900

    added ng components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BagComponent } from './bag/bag.component';
+import { ChecklistComponent } from './checklist/checklist.component';
+import { DescriptionComponent } from './description/description.component';
+import { EarthquakeComponent } from './earthquake/earthquake.component';
+import { FireComponent } from './fire/fire.component';
+import { FloodComponent } from './flood/flood.component';
+import { HomeComponent } from './home/home.component';
+import { NuclearComponent } from './nuclear/nuclear.component';
+import { TornadoComponent } from './tornado/tornado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    BagComponent,
+    ChecklistComponent,
+    DescriptionComponent,
+    EarthquakeComponent,
+    FireComponent,
+    FloodComponent,
+    HomeComponent,
+    NuclearComponent,
+    TornadoComponent
   ],
   imports: [
     BrowserModule,
